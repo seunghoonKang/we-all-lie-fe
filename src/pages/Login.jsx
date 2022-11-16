8194f246adfd668781bd3a683c5b5524116ea858
parkharoi <haha2000ab@gmail.com>
2022-11-17 04:20:16 +0900

    🤮 fix : 통신 끊기는거 해결 💋 style : spinner추가 🛠️ refactor  : 이전 페이지 방지 코드 추가
    #12

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,6 @@ import { KAKAO_AUTH_URL } from '../components/login/Oauth';
 import styled from 'styled-components';
 // import { socket } from "../shared/socket";
 import Notice from '../elements/Notice';
-import Header from '../elements/Header';
 
 // socket.emit("leave_Room", "하이하이");
 // socket.emit("enter_Room", "하이하이");
@@ -17,19 +16,44 @@ const Login = () => {
     window.location.href = KAKAO_AUTH_URL;
   };
   return (
-    <div>
+    <Wrap>
       <Notice />
-      <Header />
-      <KaKaoBtn onClick={handleLogin}>
-        <img style={{ transform: 'scale(0.3)' }} src="/img/kakao1.png"></img>
-        <span>카카오계정 로그인</span>
-      </KaKaoBtn>
-      <MainImg>메인 이미지</MainImg>
-    </div>
+      <MiniWrap>
+        <P>WE ALL LION</P>
+        <MainImg>
+          <img src="/img/lion.png"></img>
+        </MainImg>
+        <KaKaoBtn onClick={handleLogin}>
+          <img style={{ transform: 'scale(0.3)' }} src="/img/kakao1.png"></img>
+          <span>카카오계정 로그인</span>
+        </KaKaoBtn>
+      </MiniWrap>
+    </Wrap>
   );
 };
 
 export default Login;
+
+const Wrap = styled.div`
+  background-color: tomato;
+`;
+
+const MiniWrap = styled.div`
+  background-color: green;
+  position: absolute;
+  left: 50%;
+  margin-left: -50%;
+`;
+
+const P = styled.div`
+  color: #000000;
+  font-family: Pretendard, 'Source Sans Pro';
+  font-size: 50px;
+  font-weight: 800;
+  white-space: nowrap;
+  width: 28.2rem;
+  background-color: pink;
+`;
 
 const KaKaoBtn = styled.div`
   height: 52px;
@@ -42,24 +66,21 @@ const KaKaoBtn = styled.div`
   justify-content: center;
   width: 50%;
   border-radius: 8px;
-  cursor: pointer;
   font-weight: bold;
   font-size: 16px;
   line-height: 24px;
   margin-top: 32px;
-  float: right;
+  float: center;
   &:hover {
     background-color: #f4c31f;
   }
 `;
 
 const MainImg = styled.div`
-  position: absolute;
-  width: 700px;
-  height: 709px;
-  left: 0px;
-  top: 218px;
-  margin: 0px 0px 0px 20px;
-  background: rosybrown;
-  border-radius: 20px;
+  height: 360px;
+  object-fit: contain;
+  top: 188px;
+  vertical-align: top;
+  width: 1080px;
+  background-color: gray;
 `;
