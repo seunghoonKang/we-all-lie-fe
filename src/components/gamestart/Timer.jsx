8995684ce3014bc4b118475e527d78c8608941d0
seunghoon <tmdgns4321@gmail.com>
2022-11-21 00:55:45 +0900

    ☀️ feat : 타이머 컴포넌트 구현
    타이머 컴포넌트를 만들었습니다.시간 단위는 사용하지 않아 분, 초로 나누었습니다.

---
+++ src/components/gamestart/Timer.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+const padStartTime = (num) => {
+  return String(num).padStart(2, '0');
+};
+
+const Timer = ({ min, sec }) => {
+  const Min = min ? parseInt(min) : 0;
+  const Sec = sec ? parseInt(sec) : 0;
+  const count = useRef(Min * 60 + Sec);
+  const interval = useRef(null);
+
+  const [minute, setMinute] = useState(padStartTime(Min));
+  const [second, setSecond] = useState(padStartTime(Sec));
+
+  useEffect(() => {
+    interval.current = setInterval(() => {
+      count.current -= 1;
+
+      setMinute(padStartTime(parseInt((count.current % 3600) / 60)));
+      setSecond(padStartTime(count.current % 60));
+    }, 1000);
+  }, []);
+
+  useEffect(() => {
+    if (count.current <= 0) {
+      clearInterval(interval.current);
+    }
+  }, [second]);
+
+  return (
+    <div>
+      {minute} : {second}
+    </div>
+  );
+};
+
+export default Timer;
