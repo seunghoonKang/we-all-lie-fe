1879bdf265dca10288c303045c0487f04595b999
parkharoi <haha2000ab@gmail.com>
2022-11-25 17:25:27 +0900

    📝 docs : 로그인 오류 수정중

--- src/components/login/kakao.jsx
+++ src/components/login/kakao.jsx
@@ -17,14 +17,16 @@ const Kakao = () => {
   const Token = async () => {
     try {
       const res = await axios.get(
-        `http://3.36.1.72/api/auth/kakao/callback?code=${code}`
+        `https://minhyeongi.xyz/api/auth/kakao/callback?code=${code}`
+        //`http://3.36.1.72/api/auth/kakao/callback?code=${code}`
       );
       const kakaoToken = res.data.accessToken;
       console.log(kakaoToken);
       localStorage.setItem('kakaotoken', kakaoToken);
       if (res.status === 200) {
         const res2 = await axios.post(
-          `http://3.36.1.72/api/auth/kakao/callback?code=${code}`,
+          `https://minhyeongi.xyz/api/auth/kakao/callback?code=${code}`,
+          //`http://3.36.1.72/api/auth/kakao/callback?code=${code}`,
           {
             kakaoToken, //카카오 토큰
           },
