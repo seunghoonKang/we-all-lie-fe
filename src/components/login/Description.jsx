b5a3274db1e8f7b2569dab294619bf9082920772
parkharoi <haha2000ab@gmail.com>
2022-11-29 18:54:12 +0900

    💋 style : 모달 svg 수정

--- src/components/login/Description.jsx
+++ src/components/login/Description.jsx
@@ -6,17 +6,6 @@ const Description = ({ closeDescription }) => {
   const closeBtnHandler = () => {
     closeDescription();
   };
-  //   useEffect(() => {
-  //     document.body.style.cssText = `
-  //     position: fixed;
-  //     top: -${window.scrollY}px;
-  //     width: 100%;`;
-  //     return () => {
-  //       const scrollY = document.body.style.top;
-  //       document.body.style.cssText = '';
-  //       window.scrollTo(0, parseInt(scrollY || '0', 10) * -1);
-  //     };
-  //   }, []);
 
   return (
     <ModalContainer>
