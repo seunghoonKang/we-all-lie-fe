229754871c92ce0aebed92c5fee8e0af1b68e025
yoooooooung <oaoa9n@gmail.com>
2022-12-11 10:34:33 +0900

    📝 docs : 스파이가 맞출 키워드 뿌리기 수정중

--- src/components/gamevote/WordExamples.jsx
+++ src/components/gamevote/WordExamples.jsx
@@ -5,26 +5,26 @@ import styled from 'styled-components';
 
 const WordExamples = (props) => {
   const words = useSelector((state) => state.game.showWords);
-  const answerWord = useSelector((state) => state.game.answerWord);
+  const answerWord = useSelector((state) => state.game.sendCategory.answerWord);
   const category = useSelector((state) => state.game.category);
   const spy = useSelector((state) => state.game.spy); //스파이 닉네임 들고오기
   const [cookies] = useCookies(['nickname']);
   const nickname = cookies.nickname;
 
-  const spyClickWord = (word) => {
+  const spyClickWord = (a) => {
     if (spy === nickname) {
-      props.setSpyAnswer(word);
-      console.log('선택한 word', word);
+      props.setSpyAnswer(a);
+      console.log('선택한 word', a);
       console.log('spyAnswer에 적용 잘 됐나?', props.spyAnswer);
     }
   };
-
+  console.log('words 찍어보기', words);
   return (
     <>
       {/* 내가 스파이라면 onClick 적용 */}
       {spy === cookies.nickname ? (
         <>
-          {words.map((word) => {
+          {words?.map((word) => {
             <Word key={word} onClick={() => spyClickWord(word)}>
               {word}
             </Word>;
